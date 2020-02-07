392796a8585683453b49b6254069fca2259c8eb0
2Zero1 <Good7219@hotmail.com>
2020-02-07 10:37:04 +0900

    Solve draw problem by JavaScript
    Reviewed draw problem

---
+++ 20200207/draw.test.js
@@ -0,0 +1,62 @@
+/*
+    - A와 B를 2로 나눠 반올림 했을때 둘다 1이 나올 때 라운드를 리턴
+    - 위의 것을 확인한 후에 라운드에 +1을 해줌.
+*/
+
+const solution = (n,a,b) => {
+    var round = 1;
+    var target_a = next(a);
+    var target_b = next(b);
+
+    while(target_a != target_b){
+        round++;
+        target_a = next(target_a);
+        target_b = next(target_b);
+    }
+    return round;
+    if(target_a == target_b) {
+        return round;
+    }
+    round++;
+
+    target_a = next(target_a);
+    target_b = next(target_b);
+
+    if(target_a == target_b) {
+        return round;
+    }
+    round++;
+
+    target_a = next(target_a);
+    target_b = next(target_b);
+
+    if(target_a == target_b) {
+        return round;
+    }
+    round++;
+    
+    
+}
+
+const next = (a) => Math.ceil(a/2);
+const greater = (a, b) => a - b > 0 ? a : b
+const less = (a, b) => a - b < 0 ? a : b
+
+test('solution', () => {
+    expect(solution(2,1,2)).toBe(1);
+    expect(solution(2,2,1)).toBe(1);
+    expect(solution(4,3,4)).toBe(1);
+    expect(solution(4,4,3)).toBe(1);
+    expect(solution(4,1,3)).toBe(2);
+    expect(solution(4,2,4)).toBe(2);
+    expect(solution(4,4,2)).toBe(2);
+    expect(solution(8,4,7)).toBe(3);
+    expect(solution(8,7,4)).toBe(3);
+    expect(solution(8,1,5)).toBe(3);
+})
+
+//1 2 3 4 5 6 7 8
+
+// 1   3   5   7
+
+//   1       5
